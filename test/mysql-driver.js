69b862075c86776d701849d3eee97e3fb2a3a1c6
Ruben Hakopian <ruben.hakopian@gmail.com>
2020-06-14 19:47:28 -0700

    more cases

--- test/mysql-driver.js
+++ test/mysql-driver.js
@@ -3,10 +3,6 @@ const _ = require('the-lodash');
 const logger = require('the-logger').setup('test', { pretty: true });
 const MySqlDriver = require('../');
 
-// if (!process.env.MYSQL_HOST) {
-//     return;
-// }
-
 describe('mysql-driver', function() {
 
     it('constructor', function() {
@@ -29,26 +25,22 @@ describe('mysql-driver', function() {
         });
     });
 
-    // it('execute', function() {
-    //     var mysqlDriver = new MySqlDriver(logger, true);
+    it('execute', function() {
+        var mysqlDriver = new MySqlDriver(logger, true);
 
-    //     mysqlDriver.connect();
+        mysqlDriver.connect();
 
-    //     return new Promise((resolve, reject) => {
-    //         mysqlDriver.onConnect(() => {
-    //             resolve();
-    //         })
-    //     })
-    //     .then(() => {
-    //         return mysqlDriver.executeSql("SELECT table_name FROM information_schema.tables;");
-    //     })
-    //     .then(result => {
-    //         (result).should.be.an.Array();
-    //         (result.length > 1).should.be.true();
-    //     })
-    //     .then(() => {
-    //         return mysqlDriver.close();
-    //     });
-    // });
+        return mysqlDriver.waitConnect()
+        .then(() => {
+            return mysqlDriver.executeSql("SELECT table_name FROM information_schema.tables;");
+        })
+        .then(result => {
+            (result).should.be.an.Array();
+            (result.length > 1).should.be.true();
+        })
+        .then(() => {
+            return mysqlDriver.close();
+        });
+    });
 
 });
