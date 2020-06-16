400b128b7a1f353c86fe2607723f7739ec49d021
Ruben Hakopian <ruben.hakopian@gmail.com>
2020-06-15 18:38:56 -0700

    added more test cases

--- test/mysql-driver.js
+++ test/mysql-driver.js
@@ -158,6 +158,40 @@ describe('mysql-driver', function() {
         });
     });
 
+    it('prepare-statement-after-connect-error-2', function() {
+        var mysqlDriver = new MySqlDriver(logger, true);
+
+        mysqlDriver.connect();
+
+        return mysqlDriver.waitConnect()
+        .then(() => {
+
+            var selectStatement = mysqlDriver.statement("SELECT * FROM contactZ;");
+
+            var returedError = false;
+            return selectStatement.execute()
+                .then(() => selectStatement.execute())
+                .catch(reason => {
+                    returedError = true;
+                })
+                .then(() => {
+                    (returedError).should.be.true();
+                    returedError = false;
+                })
+                .then(() => selectStatement.execute())
+                .catch(reason => {
+                    returedError = true;
+                })
+                .then(() => {
+                    (returedError).should.be.true();
+                });
+
+        })
+        .then(() => {
+            return mysqlDriver.close();
+        });
+    });
+
 
     it('prepare-statement-no-connect-error', function() {
         var mysqlDriver = new MySqlDriver(logger, true);
@@ -236,5 +270,65 @@ describe('mysql-driver', function() {
                 return mysqlDriver.close();
             });
     });
+
+
+
+    it('prepare-reconnect', function() {
+        var mysqlDriver = new MySqlDriver(logger, true);
+
+        var deleteStatement = mysqlDriver.statement("DELETE FROM contacts;");
+        var insertStatement = mysqlDriver.statement("INSERT INTO contacts(`name`, `email`) VALUES(?, ?);");
+        var selectStatement = mysqlDriver.statement("SELECT * FROM contacts;");
+            
+        var returedError = false;
+
+        mysqlDriver.connect();
+        return mysqlDriver.waitConnect()
+        .then(() => {
+            return deleteStatement.execute()
+                .then(() => selectStatement.execute())
+                .then(result => {
+                    logger.info("Result: ", result);
+                    (result).should.be.an.Array();
+                    (result.length == 0).should.be.true();
+                })
+                .then(() => insertStatement.execute(['John Doe', 'john@doe.com']))
+                .then(() => selectStatement.execute())
+                .then(result => {
+                    logger.info("Result: ", result);
+                    (result).should.be.an.Array();
+                    (result.length == 1).should.be.true();
+                    (result[0].name).should.be.equal('John Doe');
+                    (result[0].email).should.be.equal('john@doe.com');
+                })
+                .then(() => mysqlDriver.close())
+                .then(() => selectStatement.execute())
+                .catch(reason => {
+                    logger.info(reason);
+                    returedError = true;
+                })
+                .then(() => {
+                    (returedError).should.be.true();
+                })
+                .then(() => {
+                    return mysqlDriver.connect();
+                })
+                .then(() => mysqlDriver.waitConnect())
+                .then(() => selectStatement.execute())
+                .then(result => {
+                    logger.info("Result: ", result);
+                    (result).should.be.an.Array();
+                    (result.length == 1).should.be.true();
+                    (result[0].name).should.be.equal('John Doe');
+                    (result[0].email).should.be.equal('john@doe.com');
+                })
+                ;
+
+        })
+        .then(() => {
+            return mysqlDriver.close();
+        });
+    });
+
 
 });
